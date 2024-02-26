60deb14b795ca8cabee18b6c22908c7f9db7f367
Zehan Khan <zkhan@hootboard.com>
2024-02-27 00:52:05 +0530

    test: 💀 test commit

--- src/components/Button/button.tsx
+++ src/components/Button/button.tsx
@@ -1,9 +1,13 @@
-import React from "react";
+import React from 'react';
 interface buttonProps {
   onClick: () => void;
 }
 
 export const Button = (props: buttonProps) => {
   const { onClick } = props;
-  return <button data-testid="button" onClick={onClick}></button>;
+  return (
+    <button data-testid="button" onClick={onClick}>
+      on click
+    </button>
+  );
 };
