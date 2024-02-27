2bbfb4849faf135f62ab0e3b66aa233a7f972d4f
Zehan Khan <zkhan@hootboard.com>
2024-02-27 10:22:23 +0530

    chore: :adhesive_bandage: add some fix in button and add keywords in workspace

--- src/components/Button/button.tsx
+++ src/components/Button/button.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 interface buttonProps {
   onClick: () => void;
+  children: React.ReactNode;
 }
 
 export const Button = (props: buttonProps) => {
-  const { onClick } = props;
+  const { onClick, children } = props;
   return (
     <button type="button" data-testid="button" onClick={onClick}>
-      on click
+      {children}
     </button>
   );
 };
