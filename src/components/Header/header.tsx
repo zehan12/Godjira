88825c957cb3556fe661f323e50bbd43b7c3be93
Zehan Khan <zkhan@hootboard.com>
2024-03-02 03:01:31 +0530

    feat: :railway_car: create header and add tailwind var

---
+++ src/components/Header/header.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { tv } from 'tailwind-variants';
+
+type HeaderProps = {
+  border?: boolean;
+  sticky?: boolean;
+};
+
+const NavbarStyles = tv({
+  base: 'w-full border-b border-transparent bg-background/95 backdrop-blur-sm',
+  variants: {
+    border: {
+      true: 'border-border'
+    },
+    sticky: {
+      true: 'sticky top-0 z-40'
+    },
+    animated: {
+      true: 'duration-slow animate-in fade-in slide-in-from-top-full'
+    }
+  }
+});
+
+export const Header = ({
+  border = true,
+  sticky = true // todo: fix browser's console warn if true
+}: HeaderProps) => {
+  return (
+    <header className={NavbarStyles({ border, sticky })}>
+      <nav className="container flex h-16 items-center justify-between">
+        <div>nav-bar</div>
+      </nav>
+    </header>
+  );
+};
